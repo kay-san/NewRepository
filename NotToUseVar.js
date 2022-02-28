f6045c7a1c3015bd6748f64e2b0a9f49af606cd8
kay-san <kay.sanjit@gmail.com>
2022-02-28 14:32:02 +0530

    Var and Let

---
+++ NotToUseVar.js
@@ -0,0 +1,17 @@
+//using var
+function print(){
+    for(var i = 1; i<= 5; i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+
+//using let
+print();
+function print1(){
+    for(let i = 1; i<= 5; i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+print1();
